fb0c3338bd07704b9ebdf85651b6300c70862b5c
Samer ABDELKAFI <samer.abdelkafi@bnpparibas.com>
2019-06-12 13:55:46 +0200

    remove unused import

--- service-consumer/src/main/java/com/mastering/spring/consumer/controller/NumberAdderController.java
+++ service-consumer/src/main/java/com/mastering/spring/consumer/controller/NumberAdderController.java
@@ -3,7 +3,6 @@ package com.mastering.spring.consumer.controller;
 import com.mastering.spring.consumer.service.RandomServiceProxy;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
