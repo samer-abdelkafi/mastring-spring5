275df7390397520be9feaa49f7e6d2c9d444d2a7
Samer ABDELKAFI <samer.abdelkafi@bnpparibas.com>
2019-06-12 16:22:39 +0200

    add Eureka Naming Service

--- service-consumer/src/main/java/com/mastering/spring/consumer/ServiceConsumerApplication.java
+++ service-consumer/src/main/java/com/mastering/spring/consumer/ServiceConsumerApplication.java
@@ -2,10 +2,12 @@ package com.mastering.spring.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.mastering.spring.consumer")
+@EnableDiscoveryClient
 public class ServiceConsumerApplication {
 
 	public static void main(String[] args) {
