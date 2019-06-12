c3e051fd679624dd6516bb81d1566cf844386679
samerabdelkafi <samer.abdelkafi@gmail.com>
2019-06-13 01:25:43 +0200

    add tracing config

--- microservice-a/src/main/java/com/mastering/spring/cloud/microservicea/config/ApplicationConfiguration.java
+++ microservice-a/src/main/java/com/mastering/spring/cloud/microservicea/config/ApplicationConfiguration.java
@@ -1,6 +1,8 @@
 package com.mastering.spring.cloud.microservicea.config;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,5 +17,11 @@ public class ApplicationConfiguration {
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+
+    @Bean
+    public Sampler defaultSampler() {
+        return Sampler.ALWAYS_SAMPLE;
     }
 }
--- service-consumer/src/main/java/com/mastering/spring/consumer/controller/NumberAdderController.java
+++ service-consumer/src/main/java/com/mastering/spring/consumer/controller/NumberAdderController.java
@@ -1,12 +1,14 @@
 package com.mastering.spring.consumer.controller;
 
 import com.mastering.spring.consumer.service.RandomServiceProxy;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
@@ -15,14 +17,24 @@ public class NumberAdderController {
 
     private RandomServiceProxy randomServiceProxy;
 
+    @HystrixCommand(fallbackMethod = "getDefaultResponse")
     @RequestMapping("/add")
     public Long add() {
-        long sum = 0;
+//        long sum = 0;
         List<Integer> numbers = randomServiceProxy.getRandomNumbers();
-        for (int number : numbers) {
-            sum += number;
-        }
+//        for (int number : numbers) {
+//            sum += number;
+//        }
+
+        long sum = numbers.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+
         log.info("Returning {}", sum);
         return sum;
+    }
+
+    public Long getDefaultResponse() {
+        return 10000L;
     }
 }
